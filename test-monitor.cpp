f3f97c12233546d97a8b2b3bae2387ae598fc655
SahanaVH7 <vhsahana3@gmail.com>
2025-09-06 13:04:11 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -5,3 +5,46 @@ TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_FALSE(vitalsOk(99, 102, 70));
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
 }
+
+TEST(Monitor, TemperatureOutOfRange)
+{
+  std::string msg;
+
+  EXPECT_FALSE(checkVitals(104, 70, 95, msg)); // too high
+  EXPECT_EQ(msg, "Temperature is critical!");
+
+  EXPECT_FALSE(checkVitals(94, 70, 95, msg)); // too low
+  EXPECT_EQ(msg, "Temperature is critical!");
+
+  EXPECT_TRUE(checkVitals(98.6, 72, 96, msg)); // normal
+}
+TEST(Monitor, PulseOutOfRange)
+{
+  std::string msg;
+
+  EXPECT_FALSE(checkVitals(98.6, 50, 95, msg)); // too low
+  EXPECT_EQ(msg, "Pulse Rate is out of range!");
+
+  EXPECT_FALSE(checkVitals(98.6, 120, 95, msg)); // too high
+  EXPECT_EQ(msg, "Pulse Rate is out of range!");
+
+  EXPECT_TRUE(checkVitals(98.6, 70, 95, msg)); // normal
+}
+
+TEST(Monitor, Spo2OutOfRange)
+{
+  std::string msg;
+
+  EXPECT_FALSE(checkVitals(98.6, 72, 85, msg)); // too low
+  EXPECT_EQ(msg, "Oxygen Saturation out of range!");
+
+  EXPECT_TRUE(checkVitals(98.6, 72, 96, msg)); // normal
+}
+
+TEST(Monitor, AllVitalsNormal)
+{
+  std::string msg;
+
+  EXPECT_TRUE(checkVitals(98.6, 72, 96, msg));
+  EXPECT_EQ(msg, ""); // no error message when ok
+}
